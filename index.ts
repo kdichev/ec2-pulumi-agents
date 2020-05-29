6161c317e44bd63a1496e3d8b8ef2becc9324dcf
Konstantin Dichev <konstantin.dichev@gmail.com>
2020-05-29 13:15:27 +0200

    add

--- index.ts
+++ index.ts
@@ -1,9 +1,53 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
+import * as pulumi from '@pulumi/pulumi'
+import * as aws from '@pulumi/aws'
+import * as awsx from '@pulumi/awsx'
+
+const config = new pulumi.Config()
+console.log(config)
+
+let group = new aws.ec2.SecurityGroup('app-security', {
+  description: 'Enable HTTP and SSH access',
+  ingress: [
+    {
+      protocol: 'tcp',
+      fromPort: 8080,
+      toPort: 8080,
+      cidrBlocks: ['0.0.0.0/0'],
+    },
+    { protocol: 'tcp', fromPort: 22, toPort: 22, cidrBlocks: ['0.0.0.0/0'] },
+  ],
+  egress: [
+    { protocol: '-1', fromPort: 0, toPort: 0, cidrBlocks: ['0.0.0.0/0'] },
+  ],
+})
+
+let userData = `#!/bin/bash
+mkdir actions-runner && cd actions-runner
+curl -O -L https://github.com/actions/runner/releases/download/v2.262.1/actions-runner-linux-x64-2.262.1.tar.gz
+tar xzf ./actions-runner-linux-x64-2.262.1.tar.gz
+./config.sh --url https://github.com/kdichev/ec2-pulumi-agents --token AC52MGJUWQVY335EGMNVT4C62D5OK
+./svc.sh install
+./svc.sh start
+`
+
+const createInstance = function (
+  name: string,
+  size: pulumi.Input<aws.ec2.InstanceType>
+) {
+  return new aws.ec2.Instance(name, {
+    tags: { Name: name },
+    instanceType: size,
+    vpcSecurityGroupIds: [group.id], // reference the group object above
+    ami: 'ami-085925f297f89fce1',
+    userData: userData, // start a simple web server
+    keyName: 'test-key',
+  })
+}
+
+const hasuraInstance = createInstance('build-agent-1', 't2.micro')
 
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+const bucket = new aws.s3.Bucket('my-bucket')
 
 // Export the name of the bucket
-export const bucketName = bucket.id;
+export const bucketName = bucket.id
