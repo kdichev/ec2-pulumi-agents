e1ad05c5904ea56ad8be332b6b66e9b9d2fcd977
Konstantin Dichev <konstantin.dichev@gmail.com>
2020-05-29 16:12:07 +0200

    add semi colons

--- index.ts
+++ index.ts
@@ -22,12 +22,12 @@ const group = new aws.ec2.SecurityGroup('app-security', {
 
 const userData = (name: string) => `
   #!/bin/bash
-  mkdir ${name} && cd ${name}
-  curl -O -L https://github.com/actions/runner/releases/download/v2.262.1/actions-runner-linux-x64-2.262.1.tar.gz
-  tar xzf ./actions-runner-linux-x64-2.262.1.tar.gz
-  ./config.sh --url https://github.com/kdichev/ec2-pulumi-agents --token AC52MGLH7WKQAKFYZ4JO4T262ENDK --unattended
+  mkdir ${name} && cd ${name};
+  curl -O -L https://github.com/actions/runner/releases/download/v2.262.1/actions-runner-linux-x64-2.262.1.tar.gz;
+  tar xzf ./actions-runner-linux-x64-2.262.1.tar.gz;
+  ./config.sh --url https://github.com/kdichev/ec2-pulumi-agents --token AC52MGLH7WKQAKFYZ4JO4T262ENDK --unattended;
   sudo ./svc.sh install;
-  sudo ./svc.sh start
+  sudo ./svc.sh start;
 
 `
 
