89bf71adf43ab7f464eb13f22619c4daa5074545
Konstantin Dichev <konstantin.dichev@gmail.com>
2020-05-29 15:20:58 +0200

    change token

--- index.ts
+++ index.ts
@@ -24,7 +24,7 @@ const userData = `#!/bin/bash
 mkdir actions-runner && cd actions-runner
 curl -O -L https://github.com/actions/runner/releases/download/v2.262.1/actions-runner-linux-x64-2.262.1.tar.gz
 tar xzf ./actions-runner-linux-x64-2.262.1.tar.gz
-./config.sh --url https://github.com/kdichev/ec2-pulumi-agents --token AC52MGIGVH3FKT6KZDVBPBK62EMAK --unattended
+./config.sh --url https://github.com/kdichev/ec2-pulumi-agents --token AC52MGLH7WKQAKFYZ4JO4T262ENDK --unattended
 sudo ./svc.sh install
 sudo ./svc.sh start
 `
