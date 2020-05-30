80362a0b3d92373d3aa8f4401227143e94de9b92
Konstantin Dichev <konstantin.dichev@gmail.com>
2020-05-30 18:53:10 +0200

    remove sudo

--- index.ts
+++ index.ts
@@ -24,15 +24,15 @@ const createAgetUserData = (name: string, token: string) =>
   `#!/bin/bash
 su ubuntu -c 'whoami;
 cd home/ubuntu;
-sudo mkdir agent && cd agent;
-sudo curl -O -L https://github.com/actions/runner/releases/download/v2.262.1/actions-runner-linux-x64-2.262.1.tar.gz;
+mkdir agent && cd agent;
+curl -O -L https://github.com/actions/runner/releases/download/v2.262.1/actions-runner-linux-x64-2.262.1.tar.gz;
 ls
-sudo tar xzf ./actions-runner-linux-x64-2.262.1.tar.gz;
+tar xzf ./actions-runner-linux-x64-2.262.1.tar.gz;
 ls
 ./config.sh --url https://github.com/kdichev/ec2-pulumi-agents --token AC52MGNGBKQCXFYPIGSPUUK62KMQM --unattended;
 ls
-sudo ./svc.sh install;
-sudo ./svc.sh start;'`
+./svc.sh install;
+./svc.sh start;'`
 
 const createInstance = (
   name: string,
